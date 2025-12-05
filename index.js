748309f6c2926750157ef7d4a5b2fe01294cfb49
jones-semicolon <marjones.lapinig@gmail.com>
2025-12-06 00:44:34 +0800

    server added

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const { Groq } = require("groq-sdk");
+require("dotenv").config();
+
+const app = express();
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY
+});
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json({limit: "50mb"}))
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+// Test endpoint
+app.get("/test", async (req, res) => {
+  const completion = await groq.chat.completions.create({
+    messages: [{ role: "system", content: "Hello" }],
+    model: "meta-llama/llama-4-maverick-17b-128e-instruct"
+  });
+  console.log(completion);
+});
+
+// Chat endpoint
+app.post("/chat", express.json(), async (req, res) => {
+  try {
+    const { image_url } = req.body;
+
+    if (!image_url) {
+      return res.status(400).json({ error: "image_url is required" });
+    }
+
+    console.log("Received image URL:", image_url);
+
+    const completion = await groq.chat.completions.create({
+      messages: [
+        { role: "system", content: process.env.CONTENT },
+        {
+          role: "user",
+          content: [
+            { type: "image_url", image_url: { url: image_url } }
+          ]
+        }
+      ],
+      model: "meta-llama/llama-4-maverick-17b-128e-instruct",
+      temperature: 1,
+      max_completion_tokens: 1024,
+      top_p: 1,
+      stream: false
+    });
+
+    const message = completion.choices[0].message?.content || "";
+
+    res.json({
+      success: true,
+      model: completion.model,
+      content: message,
+      usage: completion.usage,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
